d69be8b3e5652ac0efccc535d0f725b1b3fe2dbd
Uriel Salischiker <uriel@urielsalis.me>
2023-06-15 11:15:05 +0200

    Update auto_respond.py

--- auto_respond.py
+++ auto_respond.py
@@ -8,23 +8,38 @@ import config
 reddit = praw.Reddit(
 	client_id = config.client_id,
 	client_secret = config.client_secret,
-	user_agent="modmail_auto_responder_v0.1 by u/buckrowdy",
+	user_agent="modmail_auto_responder_v0.1 fork by u/urielsalis",
 	refresh_token =config.refresh_token,
 )
 
 # Define the list you want this to operate on.  A modmail conversation stream fetches ALL subreddits
-allowed_subreddits = ["YOUR_SUBREDDITS_HERE", "AS_A_LIST"]
+allowed_subreddits = ["minecraft"]
 
 # Define the list of keywords for the auto-respond trigger
 keywords = ['request to join','let me in', 'access', 'member', 'private', 'blackout', 'dark', 'closed', 'join', 'shutdown,']
 
 # Define the auto-response
-main_response_message =f"Hello and thank you...[YOUR_MESSAGE_HERE]" 
+main_response_message ="""Hello /u/{author},
 
-# Set up a list to hold IDs of modmail the bot has already replied to.
-processed_mail = []
+The subreddit r/Minecraft is currently set to private mode in protest of Reddit's changes to the API and will not be public again until Reddit Inc. accommodates for the Reddit community's needs. This decision was not taken lightly, but it was [voted upon by our community](https://screenshot_of_poll_result_goes_here.xyz), who decided that this would be the right thing to do.
+You can read more about this on r/Save3rdPartyApps or in [this topic](https://redd.it/1476ioa).
 
-# Begin the mnain function of the bot.  The bot will need to be interrupted manually
+In the meantime, here are some links you may need:  
+
+**Information/Support:**
+- [Minecraft Support, if you have problems with your account](https://help.minecraft.net/hc/)
+- [Minecraft Community Support Discord server, if you need tech support involving the game itself](https://discord.gg/58Sxm23)
+- [Minecraft Wiki, if you have a question about the game or are looking at how rare is a pink sheep](https://minecraft.fandom.com/)  
+- [Minecraft Bug Tracker, if you want to see if something in the game is normal or not](https://bugs.mojang.com/)
+
+**Other Minecraft social platforms:**
+- [The Minecraft Forum, if you want a place that can fill the Reddit-shaped hole in your heart](https://minecraftforum.net)
+- [The official Minecraft Discord server](https://discord.gg/minecraft)
+
+Best regards,
+The r/Minecraft Team"""
+
+# Begin the main function of the bot.  The bot will need to be interrupted manually
 while True:
     try:
         # Print something to the terminal so you know it's working.
@@ -35,14 +50,8 @@ while True:
             if conv.owner not in allowed_subreddits:
                 continue  # If not, skip the rest of this loop and move to the next conversation
          
-            # This specific condition will send a PM top every mod on the team if an admin modmail is sent so you don't miss it.
-            if len([author for author in conv.authors if author.is_admin]) > 0:
-                # Utilize u/mod_mailer, a mail relay bot.
-                reddit.redditor("mod_mailer").message(subject=f"{conv.owner}", message =f"New Admin modmail in r/{conv.owner}\n\n---\n\nNew modmail message from admins https://mod.reddit.com/mail/all/{conv.id}\n\nSubject: {conv.subject}")
-                conv.archive()
-
             # Check if we've already processed this modmail.
-            if conv.id not in processed_mail:
+            if conv.state != "archived":
                 # Grab the subject of the first message in the conversation.
                 modmail_subject = conv.subject.lower()
 
@@ -60,7 +69,6 @@ while True:
                     conv.reply(body=response_message, author_hidden=True)
                     conv.archive()
                     # Add the id of the conversation to a list so it won't be checked again.
-                    processed_mail.append(conv.id)
                     print(f"Replied to message ID {conv.id} from user {conv.user.name} with the preset response\n")
                  
     except Exception as e:
